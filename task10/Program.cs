a02080d6097c038eb074b54988b6ef66e2454ecc
buzum <buzum@mail.ru>
2023-03-03 19:23:12 +0400

    task 10 for HW 2

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите целое трёхзначное число: ");
+int userNumber = int.Parse(Console.ReadLine() ?? "");
+
+///////
+
+int secondDigit = ((userNumber - userNumber % 10) % 100) / 10;
+Console.Write(secondDigit);
