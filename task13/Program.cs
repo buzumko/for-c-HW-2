98d1f0adc672852dec64cce792356171e6b76f30
buzum <buzum@mail.ru>
2023-03-03 20:37:08 +0400

    task 13 for HW 2

---
+++ task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите целое число: ");
+string userString = Console.ReadLine() ?? "";
+int stringLenght = userString.Length;
+int userNumber = int.Parse(userString);
+
+///////
+if (stringLenght < 3)
+{
+    Console.Write("третьей цифры нет");
+}
+else
+{
+    int[] dijits = new int[stringLenght];
+    int intermediateNumber = userNumber;
+    for (int index = 0; index < stringLenght; index++)
+    {
+       dijits[index] = intermediateNumber % 10;
+        intermediateNumber = (intermediateNumber -  dijits[index]) / 10;
+    }
+    Console.Write(dijits[stringLenght - 3]);
+}
